4e57a1752d43c8c377d08326f6800d02d58ee0cd
ncorv <)@:)>
2017-11-19 23:19:49 -0500

    concurrent map writing

--- Game.go
+++ Game.go
@@ -17,6 +17,9 @@ var mutex = &sync.Mutex{}
 
 // StartGame - function will handle callback for !start, and will add a new gamestate struct to the global map
 func StartGame(s *discordgo.Session, m *discordgo.MessageCreate) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
@@ -30,25 +33,22 @@ func StartGame(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 // JoinGame - callback for when someone says !join
 func JoinGame(s *discordgo.Session, m *discordgo.MessageCreate) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
 	if m.Content == "!join" {
 		if game, ok := Games[m.ChannelID]; ok {
-			mutex.Lock()
 			game.Players = append(game.Players, m.Author.Username)
-			mutex.Unlock()
 			s.ChannelMessageSend(m.ChannelID, m.Author.Username+" has joined the game.")
 		} else {
 			var gameInstance Game
-			mutex.Lock()
 			Games[m.ChannelID] = gameInstance
-			mutex.Unlock()
 			s.ChannelMessageSend(m.ChannelID, ":wolf: A new game of Werewolf is starting! For a tutorial, type !help.\r\n\r\n")
-			mutex.Lock()
 			game.Players = append(game.Players, m.Author.Username)
-			mutex.Unlock()
 			s.ChannelMessageSend(m.ChannelID, m.Author.Username+" has joined the game.")
 		}
 	}
