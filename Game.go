bf4f09496aaee7d0ee6ceb1b1f32a97bda05a772
ncorv <)@:)>
2017-11-20 02:06:24 -0500

    repeated \!starts will no longer reallocate a new game state

--- Game.go
+++ Game.go
@@ -23,8 +23,12 @@ func StartGame(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	if m.Content == "!start" {
-		Games[m.ChannelID] = &Game{}
-		s.ChannelMessageSend(m.ChannelID, ":wolf: A new game of Werewolf is starting! For a tutorial, type !help.\r\n\r\n")
+		if Games[m.ChannelID] != nil {
+			s.ChannelMessageSend(m.ChannelID, "A game has already been started in this channel...")
+		} else {
+			Games[m.ChannelID] = &Game{}
+			s.ChannelMessageSend(m.ChannelID, ":wolf: A new game of Werewolf is starting! For a tutorial, type !help.\r\n\r\n")
+		}
 	}
 }
 
@@ -38,16 +42,29 @@ func JoinGame(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	if m.Content == "!join" {
 		if game, ok := Games[m.ChannelID]; ok {
-			game.Players = append(game.Players, m.Author.Username)
-			s.ChannelMessageSend(m.ChannelID, m.Author.Username+" has joined the game!")
+			if !sliceContainsString(game.Players, m.Author.Username) {
+				game.Players = append(game.Players, m.Author.Username)
+				s.ChannelMessageSend(m.ChannelID, m.Author.Username+" has joined the game!")
+			} else {
+				s.ChannelMessageSend(m.ChannelID, m.Author.Username+", you have already joined the game...")
+			}
 
 		} else {
 			Games[m.ChannelID] = &Game{}
 			s.ChannelMessageSend(m.ChannelID, ":wolf: A new game of Werewolf is starting! For a tutorial, type !help.\r\n\r\n")
-			game.Players = append(game.Players, m.Author.Username)
+			Games[m.ChannelID].Players = append(Games[m.ChannelID].Players, m.Author.Username)
 			s.ChannelMessageSend(m.ChannelID, m.Author.Username+" has joined the game!")
 		}
 	}
+}
+
+func sliceContainsString(stringSlice []string, searchString string) bool {
+	for _, value := range stringSlice {
+		if value == searchString {
+			return true
+		}
+	}
+	return false
 }
 
 func PrintGame(s *discordgo.Session, m *discordgo.MessageCreate) {
