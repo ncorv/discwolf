39df843a158702bdc1f92b07b1c9f2f8c58144c9
ncorv <)@:)>
2017-11-19 22:52:34 -0500

    concurrent map writing

--- Game.go
+++ Game.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"sync"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 // Game -
 type Game struct {
@@ -9,6 +13,7 @@ type Game struct {
 
 // Games -
 var Games = make(map[string]Game)
+var mutex = &sync.Mutex{}
 
 // StartGame - function will handle callback for !start, and will add a new gamestate struct to the global map
 func StartGame(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -31,13 +36,19 @@ func JoinGame(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	if m.Content == "!join" {
 		if game, ok := Games[m.ChannelID]; ok {
+			mutex.Lock()
 			game.Players = append(game.Players, m.Author.Username)
+			mutex.Unlock()
 			s.ChannelMessageSend(m.ChannelID, m.Author.Username+" has joined the game.")
 		} else {
 			var gameInstance Game
+			mutex.Lock()
 			Games[m.ChannelID] = gameInstance
+			mutex.Unlock()
 			s.ChannelMessageSend(m.ChannelID, ":wolf: A new game of Werewolf is starting! For a tutorial, type !help.\r\n\r\n")
+			mutex.Lock()
 			game.Players = append(game.Players, m.Author.Username)
+			mutex.Unlock()
 			s.ChannelMessageSend(m.ChannelID, m.Author.Username+" has joined the game.")
 		}
 	}
